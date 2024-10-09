8ee237a17ac882dc0a7410492835cb2809626235
devpsicoamgg <psico.amgg@gmail.com>
2024-10-09 18:37:50 -0500

    Created assets/images, constants.py and main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
-import sys
 import constants
+import sys
 
-#Inicialización de pygame
+# Inicialización de PyGame
 pygame.init()
 
-# Crea la ventana
+# Configuración de la pantalla
 screen = pygame.display.set_mode((constants.DIMENSIONS_WINDOW))
 pygame.display.set_caption('Believe 🌟 & Achieve 🪜 with Kodland')
 
-# Bucle  
+
+
+# Bucle del juego
 run = True
-while run:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
             sys.exit()
+    
+
